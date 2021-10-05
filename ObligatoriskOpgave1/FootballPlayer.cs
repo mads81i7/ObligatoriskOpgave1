5bc0db12b2a452cadfedc5b9e21c507dc5ba1e5d
Mads Horsted Brunebjerg <mads81i7@edu.easj.dk>
2021-10-05 10:10:26 +0200

    Add project files.

---
+++ ObligatoriskOpgave1/FootballPlayer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Security.Principal;
+
+namespace ObligatoriskOpgave1
+{
+    public class FootballPlayer
+    {
+        private string _name;
+        private double _price;
+        private int _shirtNumber;
+
+        public int Id { get; set; }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) throw new ArgumentOutOfRangeException();
+                {
+                    _name = value;
+                }
+            }
+        }
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value <= 0) throw new ArgumentOutOfRangeException();
+                {
+                    _price = value;
+                }
+            }
+        }
+
+        public int ShirtNumber
+        {
+            get => _shirtNumber;
+            set
+            {
+                if (value < 1 || value > 100) throw new ArgumentOutOfRangeException();
+                {
+                    _shirtNumber = value;
+                }
+            }
+        }
+    }
+}
---
+++ ObligatoriskOpgave1Tests/FootballPlayerTests.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ObligatoriskOpgave1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObligatoriskOpgave1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private FootballPlayer _footballPlayer;
+
+        [TestInitialize]
+        public void MyTestInitialize()
+        {
+            _footballPlayer = new FootballPlayer
+            {
+                Id = 1, Name = "Hans", Price = 20, ShirtNumber = 5
+            };
+        }
+        [TestMethod()]
+        public void NameTest()
+        {
+            _footballPlayer.Name = "Nikolaj";
+            Assert.AreEqual("Nikolaj", _footballPlayer.Name);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.Name = "Bo");
+        }
+
+        [TestMethod()]
+        public void PriceTest()
+        {
+            _footballPlayer.Price = 0.1;
+            Assert.AreEqual(0.1, _footballPlayer.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.Price = 0);
+        }
+
+        [TestMethod()]
+        public void ShirtNumberTest()
+        {
+            _footballPlayer.ShirtNumber = 100;
+            Assert.AreEqual(100, _footballPlayer.ShirtNumber);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => _footballPlayer.ShirtNumber = 0);
+        }
+
+        [TestMethod()]
+        public void IdTest()
+        {
+            Assert.AreEqual(1, _footballPlayer.Id);
+        }
+    }
+}
